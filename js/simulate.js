954256aa4748a19fe73b1f988538d10f4097cf98
Johannes Emmoth <johannes.emmoth@gmail.com>
2023-02-18 13:45:31 +0100

    Begun creating my heuristic

--- js/simulate.js
+++ js/simulate.js
@@ -1,6 +1,5 @@
 function Simulation() {
     this.game = window.game;
-    this.board = this.game.serialize();
     this.spawnChance2 = document.querySelector('#spawn-chance-2').value;
     this.autoPlay = false;
     this.optimalMove = null;
@@ -11,6 +10,8 @@ function Simulation() {
         DOWN: 2,
         LEFT: 3
     };
+
+    this.maxDepth = 3;
 
     this.init();
 }
@@ -53,8 +54,127 @@ Simulation.prototype.changeSpawnChance = function (e) {
     this.spawnChance2 = e.target.value;
 };
 
-Simulation.prototype.getNextMove = function () {
+/*
+{
+    "grid": {
+        "size": 4,
+        "cells": [
+            [
+                {
+                    "position": {
+                        "x": 0,
+                        "y": 0
+                    },
+                    "value": 2
+                },
+                null,
+                null,
+                null
+            ],
+            [
+                null,
+                null,
+                null,
+                null
+            ],
+            [
+                null,
+                null,
+                null,
+                {
+                    "position": {
+                        "x": 2,
+                        "y": 3
+                    },
+                    "value": 2
+                }
+            ],
+            [
+                null,
+                null,
+                null,
+                null
+            ]
+        ]
+    },
+    "score": 0,
+    "over": false,
+    "won": false,
+    "keepPlaying": false
+}
+ */
 
+Simulation.prototype.generateScore = function (board, currentDepth) {
+    if(currentDepth >= this.maxDepth) {
+        return calculateFinalScore(board);
+    }
+
+    let totalScore = 0;
+    return totalScore; // TODO: Calculate the total score
+};
+
+Simulation.prototype.areBoardsEqual = function (a, b) {
+    if(a === b) return true;
+    if(a === null || b === null) return false;
+
+    for(let i = 0; i < 4; j++) {
+        for(let j = 0; j < 4; j++) {
+            if(a[i][j] === null && b[i][j] !== null) {
+                return false;
+            }
+
+            if(a[i][j] !== null && b[i][j] === null) {
+                return false;
+            }
+
+            if(a[i][j].value !== b[i][j].value) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
+
+Simulation.prototype.simulateMove = function (board, move) {
+    // TODO: Simulate the move
+    return this.normalizeBoard(board);
+};
+
+Simulation.prototype.calculateScore = function (board, move) {
+  const newBoard = this.simulateMove(board, move);
+
+  if(this.areBoardsEqual(board, newBoard)) {
+      return 0;
+  }
+
+  return this.generateScore(newBoard, 0);
+};
+
+Simulation.prototype.normalizeBoard = function (board) {
+    return board; // TODO: Normalize the board
+};
+
+Simulation.prototype.getCurrentBoard = function ()  {
+    const board = this.game.serialize().grid.cells;
+    return this.normalizeBoard(board);
+};
+
+Simulation.prototype.getNextMove = function () {
+    const currentBoard = this.getCurrentBoard();
+
+    let bestMove = null;
+    let bestScore = 0;
+
+    for(let move in Object.values(this.Keys)) {
+        const score = this.calculateScore(currentBoard, move);
+        if(score > bestScore) {
+            bestScore = score;
+            bestMove = move;
+        }
+    }
+
+    return bestMove;
 };
 
 Simulation.prototype.updateOptimalMoveText = function () {
